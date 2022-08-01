8ce78c20000d5cd8b4173979c8c85f023c5a2934
Juliet Zhang <julietzhang3@gmail.com>
2022-07-31 17:56:36 -0700

    Add Template schema, update User and Section schemas

---
+++ server/controllers/templateController.js
@@ -0,0 +1,9 @@
+const { Project } = require("../db");
+const { User } = require("../db");
+const { Section } = require("../db");
+
+// TODO
+
+module.exports = {
+    // TODO
+};
---
+++ server/routes/api/templateRouter.js
@@ -0,0 +1,12 @@
+const Router = require("express-promise-router");
+const { read } = require("fs");
+
+const {
+    // TODO
+} = require("../../controllers/templateController");
+
+const router = new Router();
+
+// TODO
+
+module.exports = router;
